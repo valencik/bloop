cd34788fbaa04ed267536bf84876505f7f11cdbf
Martin Duhem <martin.duhem@gmail.com>
2018-02-13 16:29:48 +0100

    Reload projects when configurations change
    Fixes #261

--- frontend/src/main/scala/bloop/engine/caches/StateCache.scala
+++ frontend/src/main/scala/bloop/engine/caches/StateCache.scala
@@ -5,10 +5,16 @@ import java.util.concurrent.ConcurrentHashMap
 
 import bloop.engine.State
 final class StateCache(cache: ConcurrentHashMap[AbsolutePath, State]) {
-  def getStateFor(path: AbsolutePath): Option[State] = Option(cache.get(path))
   def updateBuild(state: State): State = cache.put(state.build.origin, state)
-  def addIfMissing(from: AbsolutePath, computeBuild: AbsolutePath => State): State =
-    cache.computeIfAbsent(from, p => computeBuild(p))
+  def addIfMissing(from: AbsolutePath, computeBuild: AbsolutePath => State): State = {
+    val state = cache.computeIfAbsent(from, p => computeBuild(p))
+    if (!state.build.changed) state
+    else {
+      val updatedState = computeBuild(from)
+      val _ = cache.put(from, updatedState)
+      updatedState
+    }
+  }
   def allStates: Iterator[State] = {
     import scala.collection.JavaConverters._
     cache.asScala.valuesIterator
